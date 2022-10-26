4181f312515bf73185e9f0dca63a71c75ca6f012
n.polyakova <natli.d26.l8@gmail.com>
2022-10-26 06:51:29 +0300

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,18 @@
+public class Cat {
+
+    String name;
+    private int age;
+
+    public void meow() {
+        System.out.println("Meow!");
+    }
+
+    public int getAge() {
+        return age + 1;
+    }
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
